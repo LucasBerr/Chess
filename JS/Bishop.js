18e032b295e43c58af4ab2a39fa1f2bfcc5e0341
Lucas Tabelli Berr <68754446+LucasBerr@users.noreply.github.com>
2023-10-23 19:02:10 -0300

    Arrumando movimentação do Bispo

--- JS/Bishop.js
+++ JS/Bishop.js
@@ -1,78 +1,75 @@
 import { Piece } from "./Piece.js";
 
 export class Bishop extends Piece {
-    constructor (id) {
-        super (id);
-    }
+  constructor(id) {
+    super(id);
+  }
 
-    initial_position () {
-        if (this.id[0] === "w") {
-            switch(this.id[2]) {
-                case "1":
-                    return "C1";
-                    break;
-                case "2":
-                    return "F1";
-                    break;
+  initial_position() {
+    if (this.id[0] === "w") {
+      switch (this.id[2]) {
+        case "1":
+          return "C1";
+          break;
+        case "2":
+          return "F1";
+          break;
+      }
+    } else {
+      switch (this.id[2]) {
+        case "1":
+          return "C8";
+          break;
+        case "2":
+          return "F8";
+          break;
+      }
+    }
+  }
+
+  move(boardPieces, onMove) {
+    super.clearIndicators(boardPieces);
+    if (this.color === onMove[0]) {
+      const currentLetter = this.position[0];
+      const currentNumber = parseInt(this.position[1]);
+
+      // Defina as direções de movimento (diagonal)
+      const directions = [
+        [-1, 1], // Movimento superior esquerda
+        [1, 1], // Movimento superior direita
+        [-1, -1], // Movimento inferior esquerda
+        [1, -1], // Movimento inferior direita
+      ];
+
+      for (const [dx, dy] of directions) {
+        let newLetter = currentLetter;
+        let newNumber = currentNumber;
+        let movements = [];
+
+        while (true) {
+          newLetter = String.fromCharCode(newLetter.charCodeAt(0) + dx);
+          newNumber += dy;
+
+          const newPosition = newLetter + newNumber;
+          if (!super.isValidSquare(newPosition)) {
+            break; // Sai do loop se estiver fora do tabuleiro
+          }
+          if (super.isPieceThere(boardPieces, newPosition)) {
+            const pieceInHouse = super.whichPieceIs(boardPieces, newPosition);
+            if (pieceInHouse[0] !== this.color) {
+              // Adicione apenas se a casa contiver uma peça de cor diferente
+              movements.push(newPosition);
             }
-        } else {
-            switch(this.id[2]) {
-                case "1":
-                    return "C8";
-                    break;
-                case "2":
-                    return "F8";
-                    break;
-            }
+            break; // Sai do loop se encontrar uma peça (amiga ou inimiga)
+          } else {
+            // Adicione a casa como um movimento possível
+            movements.push(newPosition);
+          }
         }
+
+        // Crie indicadores de movimento para cada movimento possível
+        super.createMoveIndicators(movements, boardPieces, onMove);
+      }
     }
-
-    move(boardPieces, onMove) {
-        super.clearIndicators(boardPieces);
-        if (this.color === onMove[0]) {
-            const currentLetter = this.position[0];
-            const currentNumber = parseInt(this.position[1]);
-            const movements = [];
-
-            // Defina as direções de movimento diagonal (superior esquerda, superior direita, inferior esquerda, inferior direita)
-            const directions = [
-                [-1, 1],   // Movimento superior esquerda
-                [1, 1],    // Movimento superior direita
-                [-1, -1],  // Movimento inferior esquerda
-                [1, -1]    // Movimento inferior direita
-            ];
-
-            for (const [dx, dy] of directions) {
-                let newLetter = currentLetter;
-                let newNumber = currentNumber;
-
-                while (true) {
-                    newLetter = String.fromCharCode(newLetter.charCodeAt(0) + dx);
-                    newNumber += dy;
-
-                    const newPosition = newLetter + newNumber;
-
-                    if (!super.isValidSquare(newPosition)) {
-                        break; // Sai do loop se estiver fora do tabuleiro
-                    }
-
-                    if (super.isPieceThere(boardPieces, newPosition)) {
-                        const pieceInHouse = super.whichPieceIs(boardPieces, newPosition);
-                        if (pieceInHouse[0] !== this.color) {
-                            // Adicione apenas se a casa contiver uma peça de cor diferente
-                            movements.push(newPosition);
-                        }
-                        break; // Sai do loop se encontrar uma peça (amiga ou inimiga)
-                    } else {
-                        // Adicione a casa como um movimento possível
-                        movements.push(newPosition);
-                    }
-                }
-            }
-
-            // Crie indicadores de movimento para cada movimento possível
-            super.createMoveIndicators(movements, boardPieces, onMove);
-        }
-    }
-
+  }
 }
